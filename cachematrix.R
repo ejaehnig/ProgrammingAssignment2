fae7351427b49b178e2a77bec9479652a68873a5
ejaehnig <ejaehnig@ucsd.edu>
2014-05-25 15:02:04 -0700

    cachematrix.R has been updated to include R code for the respective functions contained therein

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions determine if matrix has already been inverted (saved in MatrixCache);
+## If not, matrix is inverted using the solve function and saved in MatrixCache;
+## If so, matrix inversion is skipped and MatrixCache is returned instead.
 
-## Write a short comment describing this function
+## makeCacheMatrix is used to set and get MatrixCache, 
+## which stores an inverted matrix as the cache, or is NULL if the cache has not been set
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     MatrixCache <- NULL
+     set <- function (y){
+          x <<- y
+          MatrixCache <<- NULL
+     }
+     get <- function() x
+     setmatrix <- function(InvMatrix) MatrixCache <- InvMatrix
+     getmatrix <- function() MatrixCache
+     list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calls makeCacheMatrix's getmatrix subfunction to check if MatrixCache has been set
+## if it has, MatrixCache is returned; if not, the matrix is inverted using the solve function
+## and makeCacheMatrix's setmatrix subfunction is used to set MatrixCache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     MatrixCache <- x$getmatrix()
+     if(!is.null(MatrixCache)){
+          message("getting cached data")
+          return(MatrixCache)
+     }
+     data <- x$get()
+     MatrixCache <- solve(data, ...)
+     x$setmatrix(MatrixCache)
+     MatrixCache
 }
